903971a6da8822537bd5d036814c891918e05c05
Sergio Montiel <sergiomontielcastello@gmail.com>
2023-12-18 20:54:17 -0600

    Estos comandos para crear un producto en stripe, un precio y un customer, ahora hay que hacer que lo podamos crear a traves de nuestra app

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,3 +2,9 @@ require 'stripe'
 
 # Rails.env("STRIPE_SECRET_KEY")
 Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
+
+# En consola tenemos los siguientes comandos:
+
+# Stripe::Product.create({name: "Mi producto"})
+# Stripe::Price.create({product: producto.id, unit_amount: 1000, currency: "usd"}), 
+# Stripe::Customer.create({email: "demo@admin.com"}
